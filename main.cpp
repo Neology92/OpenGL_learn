14fc95619437874ee5ac25223ce8b56dd04f4053
Neology92 <oskarlegner@gmail.com>
2019-02-24 20:27:02 +0100

    Cleared main, to go with tut

--- main.cpp
+++ main.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
-// #include <GL/glew.h>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <math.h>
 
 using namespace std;
 
 int main(void)
 {
-
-	if (!glfwInit())
-	{
+	// Init GLFW
+	if (!glfwInit()){
 		exit(EXIT_FAILURE);
+		fprintf(stderr, "Problem with GLFW!");
+	}
+	// Init GLEW
+	if (glewInit()==GLEW_OK){
+		exit(EXIT_FAILURE);
+		fprintf(stderr, "Problem with GLEW!");
 	}
 
+	// Let's create window
+	GLFWwindow *window = glfwCreateWindow(640, 480, "So clear", NULL, NULL );
 
-	GLFWwindow *window = glfwCreateWindow(1000, 480, "So clear", NULL, NULL );
-
-	if(!window)
-	{
+	if(!window){
 		glfwTerminate();
 		exit(EXIT_FAILURE);
 	}
+
 
 	glfwMakeContextCurrent(window);
 	glfwSwapInterval(1);
@@ -30,7 +34,6 @@ int main(void)
 	{
 
 		glClear(GL_COLOR_BUFFER_BIT);
-
 
 
 		//Swap buffers and taking events
